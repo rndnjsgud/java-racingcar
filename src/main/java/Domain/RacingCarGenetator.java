ce13a8cb324ee2887d9eb7140f011a60623b5d4b
rndnjsgud <00kwh@naver.com>
2024-03-25 20:15:36 +0900

    Implement: Implement Domain.RacingCarGenerator

---
+++ src/main/java/Domain/RacingCarGenetator.java
@@ -0,0 +1,26 @@
+package Domain;
+
+import View.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingCarGenetator {
+    private final List<RacingCar> racingCars = new ArrayList<>();
+
+    public RacingCarGenetator(){
+    }
+
+    public RacingCar generateRacingCar(String carName){
+        return new RacingCar(carName);
+    }
+
+    public List<RacingCar> generateCars(){
+        List<String> carNames = Input.inputCarName();
+        List<RacingCar> racingCars = new ArrayList<>();
+        for(int i = 0 ; i < carNames.size() ; i++){
+            racingCars.add(generateRacingCar(carNames.get(i)));
+        }
+        return racingCars;
+    }
+}
