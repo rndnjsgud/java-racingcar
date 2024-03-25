7cda743926d5d9e4f444a66072240c8e53b5aadf
rndnjsgud <00kwh@naver.com>
2024-03-25 20:53:01 +0900

    Implement: Implement method to find winner's position

--- src/main/java/Controller/RacingCarController.java
+++ src/main/java/Controller/RacingCarController.java
@@ -21,4 +21,5 @@ public class RacingCarController {
         Output.printResults(racingCars);
 
     }
+
 }
--- src/main/java/View/Output.java
+++ src/main/java/View/Output.java
@@ -10,19 +10,36 @@ import java.util.Map;
 
 public class Output {
     private static final String RESULT = "실행결과\n";
-
+    private static final String FINAL_RESULT = "가 최종 우승했습니다";
     public static void printResults(RacingCars racingCars){
         System.out.println(RESULT);
        List<RacingCar> racingCarList = racingCars.getRacingCars();
        racingCarList.stream()
                        .forEach(cars -> System.out.println(cars.getCarName() + " : " + "-".repeat( cars.getCarPosition())));
     }
-/*
+
+    public int findWinnersPosition(RacingCars racingCars){
+        List<RacingCar> racingCarList = racingCars.getRacingCars();
+        int maxPosition = 0;
+        for(int i = 0 ; i < racingCarList.size() ; i++){
+            maxPosition = findBigger(racingCarList.get(i).getCarPosition(), maxPosition);
+        }
+        return maxPosition;
+    }
+
+    public int findBigger(int n, int m){
+        if(n > m){
+            return n;
+        }
+        return m;
+    }
+
     public static void printFinalResult(RacingCars racingCars){
         List<RacingCar> racingCarList = racingCars.getRacingCars();
-        int maxPosition = racingCarList.getCar
-        System.out.println("\b\b" + "가 최종 우승했습니다");
+        int maxPosition = 0;
+        for(int i = 0 ; i < racingCarList.size() ; i++){
 
-
-    }*/
+        }
+        System.out.println("\b\b" + FINAL_RESULT);
+    }
 }
