1ad7b11c488268f74afee5f4ab45fce5dd4fe9bb
rndnjsgud <00kwh@naver.com>
2024-03-25 18:56:24 +0900

    Implement: Inplement View.input.inputCarName

--- src/main/java/View/Input.java
+++ src/main/java/View/Input.java
@@ -1,16 +1,22 @@
 package View;
 
+import Domain.RacingCar;
 import Domain.RacingCars;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Input {
     private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분";
     private static final String INPUT_GAME_NUMBER = "시도할 횟수는 몇 회인가요?";
 
-    public void inputCarName(RacingCars racingCars){
+    public List<String> inputCarName(){
         Scanner sc = new Scanner(System.in);
-
+        List<String> carNames = Arrays.stream(sc.next()
+                .split(","))
+                .toList();
+        return carNames;
     }
-
 }
